cc530ae48ee693a3eb157abb6d3fe79823612417
Nick <1996_nick@live.nl>
2019-01-28 13:06:45 +0100

    Add files via upload

---
+++ weerSimulator/src/weerSimulator/ServerTest.java
@@ -0,0 +1,81 @@
+package weerSimulator;
+
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerTest {
+    public static void main(String[] args) {
+
+        ServerSocket server = null;
+        try {
+            server = new ServerSocket(7789);
+            server.setReuseAddress(true);
+            // The main thread is just accepting new connections
+            while (true) {
+                Socket client = server.accept();
+                System.out.println("New client connected " + client.getInetAddress().getHostAddress());
+                ClientHandler clientSock = new ClientHandler(client);
+
+                // The background thread will handle each client separately
+                new Thread(clientSock).start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (server != null) {
+                try {
+                    server.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private static class ClientHandler implements Runnable {
+
+        private final Socket clientSocket;
+
+        public ClientHandler(Socket socket) {
+            this.clientSocket = socket;
+        }
+
+        @Override
+        public void run() {
+            PrintWriter out = null;
+            BufferedReader in = null;
+      
+            try {
+                out = new PrintWriter("file.xml");
+                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                
+                String line;
+                while ((line = in.readLine()) != null) {
+                    System.out.printf("Sent from the client: %s\n", line);
+                    out.println(line);
+                    out.write(line);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (out != null) {
+                        out.close();
+                    }
+                    if (in != null)
+                        in.close();
+                    clientSocket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
